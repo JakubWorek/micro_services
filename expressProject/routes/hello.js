25b35cfed340648574bd785fa0567701ef8b8e2a
Jakub Worek <worekkuba@gmail.com>
2023-11-24 23:22:28 +0100

    minor changes

--- expressProject/routes/hello.js
+++ expressProject/routes/hello.js
@@ -4,7 +4,7 @@ var router = express.Router();
 router.get('/:name', function(req, res, next) {
     const name = req.params.name;
     res.status(200).json({
-        message: `Hello ${name}`
+        message: `Hello ${name}, my name is Jakub Worek, nice to meet you`
     });
 });
 
